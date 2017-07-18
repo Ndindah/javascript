3df1d86b6be0d6650f5b4df79c983fdac484aac8
Keem <keem@localhost.localdomain>
2017-07-18 12:07:35 +0300

    changes by Kim

---
+++ shapes.js
@@ -0,0 +1,25 @@
+function setup(){
+
+createCanvas(700,500);
+background(0);
+}
+
+// function draw(){
+// strokeWeight(40);
+// stroke(255);
+// point(350,250);
+// stroke(255,0,0);
+// strokeWeight(10);
+// line(350,250,600,400);
+// stroke(79,0,200);
+// quad(200,150,250,100,450,150,700,300);
+// /*strokeWeight(20);
+// fill(0,255,0);
+// rect(100,50,200,50);
+// fill(255,0,255);
+// rect(100,350,100,100);
+// triangle();
+// fill(0);*/
+// ellipse(400,200,150,150);
+// console.log("ellipse");
+// }
